327be4b4ec27618a3837a4431620d625530ef8b1
EmeraldBlock <yygengjunior@gmail.com>
2021-10-02 16:48:10 -0500

    remove dist to build artifact

--- dist/time.js
+++
@@ -1,9 +0,0 @@
-var Time;
-(function (Time) {
-    Time[Time["MILLI"] = 1] = "MILLI";
-    Time[Time["SECOND"] = 1000] = "SECOND";
-    Time[Time["MINUTE"] = 60000] = "MINUTE";
-    Time[Time["HOUR"] = 3600000] = "HOUR";
-})(Time || (Time = {}));
-export default Time;
-//# sourceMappingURL=time.js.map
