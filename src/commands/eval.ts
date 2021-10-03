255517121647cfefe44dc06328c419980b94fb2f
EmeraldBlock <yygengjunior@gmail.com>
2021-10-02 22:02:55 -0500

    move owners to secrets

--- src/commands/eval.ts
+++ src/commands/eval.ts
@@ -1,6 +1,6 @@
 import { Command } from "../index.js";
 
-import config from "../config/config.json";
+import secrets from "../config/secrets.json";
 
 export default new Command({
     name: "eval",
@@ -9,7 +9,7 @@ export default new Command({
 `Evaluates arbitrary JS code using eval(). Requires being an owner of the bot. USE WITH CAUTION!`,
     usage:
 `<code>`,
-    auth: message => config.owners.includes(message.author.id),
+    auth: message => secrets.owners.includes(message.author.id),
     execute: async (message, args) => {
         await message.channel.send(await eval(args.join(" ")), { code: true });
     },
